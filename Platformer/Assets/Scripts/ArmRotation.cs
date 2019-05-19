5afb5f6ae923062e92d721a8888ba6730eb05865
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-05-19 00:22:45 -0700

    Added a new player sprite from Brackeys. The player now has an arm which is not rotated when the player's body rotated. It rotated freely so that the user can can full control over the arm rotation using the mouse pointer. I also made my scripts look a little bit neater

---
+++ Platformer/Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour
+{
+    [SerializeField] int rotationOffset = 0;
+
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();     // makes all sum of vector = to 1;
+
+        float rotationZ         = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //finding the angle in degrees
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + rotationOffset);
+    }
+}
