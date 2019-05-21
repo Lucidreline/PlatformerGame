0e6ac8e0db07546c18f37ced64c1a3d419b93900
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-05-20 22:37:08 -0700

    Swapped out the Cinemachine for a script that I imported from Brackeys. I DID NOT WRITE IT. This new script follows a game object but most importantly it gives me the freedom to clamp the camera so i can stop it from going below a certain point. I also coded along a player script that gives the player health and then kills the player when health reaches 0. When you fall below a certain point, then it will kill the player aswell

---
+++ Platformer/Assets/GameMaster.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    //put static so you wont have to do a 'find object of type Game Master'
+    public static void KillPlayer(Player player) {
+        Destroy(player.gameObject);
+    }
+}
